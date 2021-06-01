d22b33546a10a56e9bab44dbd84fbbd4df939f9e
Alexandre <axiloxio@gmail.com>
2021-06-01 13:51:25 +0200

    bouton en plus

--- book7/src/sample/gui/view/ToolBoxView.java
+++ book7/src/sample/gui/view/ToolBoxView.java
@@ -2,9 +2,13 @@ package sample.gui.view;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import sample.model.Toolbox;
+import sample.model.Enums.FontStyle;
 
 public class ToolBoxView extends VBox {
     // Attributs
@@ -23,23 +27,6 @@ public class ToolBoxView extends VBox {
     // Methodes
     private void updateView() {
         // Palette de couleur
-
-        // policeTexte (Liste déroulante)
-
-        // taillePolice (Liste déroulante)
-
-        // gras (bouton)
-
-        // italique (bouton)
-
-        // soulignement (bouton)
-
-        // Exemples
-        Button policeButton = new Button();
-        policeButton.setText("Police");
-
-        getChildren().add(policeButton);
-
         Button colorButton = new Button();
         colorButton.setText("Get Color");
         colorButton.setOnAction((event) -> {
@@ -55,6 +42,56 @@ public class ToolBoxView extends VBox {
         });
         getChildren().add(colorRedButton);
 
+        // policeTexte (Liste déroulante)
+        ComboBox policeButton = new ComboBox();
+        for (int i = 0; i < FontStyle.values().length; i++) {
+            policeButton.getItems().add(FontStyle.values()[i]);;
+        }
+        policeButton.getSelectionModel().select(0);
+        getChildren().add(policeButton);
+
+        // taillePolice (Liste déroulante)
+        ComboBox tailleButton = new ComboBox();
+        for (int i = 4; i <= 25; i++) {
+            tailleButton.getItems().add(i);
+        }
+        tailleButton.getSelectionModel().select(8);
+        getChildren().add(tailleButton);
+
+        // gras (bouton)
+        Button grasButton = new Button();
+        grasButton.setText("Set gras");
+        grasButton.setOnAction((event) ->{
+            toolbox.setGras(Boolean.TRUE);
+            //text1.setStyle("-fx-font-weight: bold");
+        });
+        getChildren().add(grasButton);
+
+        // italique (bouton)
+        Button italiqueButton = new Button();
+        italiqueButton.setText("Set italique");
+        italiqueButton.setOnAction((event) ->{
+
+        });
+        getChildren().add(italiqueButton);
+
+        // soulignement (bouton)
+        Button soulignementButton = new Button();
+        soulignementButton.setText("Set soulignement");
+        soulignementButton.setOnAction((event) ->{
+
+        });
+        getChildren().add(soulignementButton);
+
+        // surlignement (bouton)
+        Button surlignementButton = new Button();
+        surlignementButton.setText("Set surlignement");
+        surlignementButton.setOnAction((event) ->{
+
+        });
+        getChildren().add(surlignementButton);
+
+        // formes
         Button shapeButton = new Button();
         shapeButton.setText("Shape");
 
