2b405490b079e9af19702a298d21c9d1dde22dce
Clement Torti <clement.torti@hotmail.fr>
2021-06-13 15:38:53 +0200

    Ajout du main permttant de creer l'artecfact

---
+++ book7/src/sample/Main_1.java
@@ -0,0 +1,12 @@
+package sample;
+
+import sample.Main;
+
+/**
+ * Corrige un bug d'intelliJ pour exporter en .jar
+ */
+public class Main_1 {
+    public static void main(String[] args) {
+        Main.main(args);
+    }
+}
